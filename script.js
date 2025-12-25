8897aac28836a8b8f3803d33f673cdc3770dfd2a
Aditya Gupta <167827368+acedroger123@users.noreply.github.com>
2025-12-25 16:02:01 +0530

    main

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ document.getElementById("registerForm").addEventListener("submit", async (event)
 
   const name = document.getElementById("name").value.trim()
   const email = document.getElementById("email").value.trim()
-  const password = document.getElementById("password").value
-  const confirmPassword = document.getElementById("confirmPassword").value
+  const password = document.getElementById("password").value.trim()
+  const confirmPassword = document.getElementById("confirmPassword").value.trim()
 
   clearErrors()
 
